89b4faa03d279ea35c459b3ec7dfb91e13a01b97
Thomas Wolfe <tewolfe2@gmail.com>
2016-07-13 09:30:50 -0700

    bsync updated

--- gulpfile.js
+++ gulpfile.js
@@ -42,7 +42,7 @@ let paths = {
 //content of task (optional)
 //})
 
-gulp.task('default', ['build', 'watch','browser-sync'], function() {
+gulp.task('default', ['build', 'watch','serve'], function() {
 
   console.log('default');
 
@@ -128,50 +128,69 @@ gulp.task('watch:css', function() {
 
 ///////browser sync///////
 
-gulp.task('nodemon', function (cb) {
-  var called = false;
-  return nodemon({
+// gulp.task('nodemon', function (cb) {
+//   var called = false;
+//   return nodemon({
 
-    // nodemon our expressjs server
-    script: 'app.js',
+//     // nodemon our expressjs server
+//     script: 'app.js',
 
-    // watch core server file(s) that require server restart on change
-    watch: ['app.js']
-  })
-    .on('start', function onStart() {
-      // ensure start only got called once
-      if (!called) { cb(); }
-      called = true;
-    })
-    .on('restart', function onRestart() {
-      // reload connected browsers after a slight delay
-      setTimeout(function reload() {
-        browserSync.reload({
-          stream: false
-        });
-      }, BROWSER_SYNC_RELOAD_DELAY);
-    });
-});
+//     // watch core server file(s) that require server restart on change
+//     watch: ['app.js']
+//   })
+//     .on('start', function onStart() {
+//       // ensure start only got called once
+//       if (!called) { cb(); }
+//       called = true;
+//     })
+//     .on('restart', function onRestart() {
+//       // reload connected browsers after a slight delay
+//       setTimeout(function reload() {
+//         browserSync.reload({
+//           stream: false
+//         });
+//       }, BROWSER_SYNC_RELOAD_DELAY);
+//     });
+// });
 
 
-gulp.task('browser-sync', ['nodemon'], function () {
+// gulp.task('browser-sync', ['nodemon'], function () {
 
-  // for more browser-sync config options: http://www.browsersync.io/docs/options/
-  browserSync({
+//   // for more browser-sync config options: http://www.browsersync.io/docs/options/
+//   browserSync({
 
-    // informs browser-sync to proxy our expressjs app which would run at the following location
+//     // informs browser-sync to proxy our expressjs app which would run at the following location
+//     proxy: 'http://localhost:8000',
+
+//     // informs browser-sync to use the following port for the proxied app
+//     // notice that the default port is 3000, which would clash with our expressjs
+//     port: 4000,
+
+//     // open the proxied app in chrome
+//     browser: ['google-chrome']
+//   });
+// });
+
+
+// gulp.task('bs-reload', function () {
+//   browserSync.reload();
+// });
+
+
+
+
+
+
+gulp.task('serve', ['nodemon'], function () {
+  browserSync.init({
     proxy: 'http://localhost:8000',
-
-    // informs browser-sync to use the following port for the proxied app
-    // notice that the default port is 3000, which would clash with our expressjs
-    port: 4000,
-
-    // open the proxied app in chrome
-    browser: ['google-chrome']
+    files: ['/public/**/*.*']
   });
 });
 
 
-gulp.task('bs-reload', function () {
-  browserSync.reload();
+gulp.task('nodemon', function () {
+  return nodemon({
+    ignore: ['./client', './public']
+  });
 });
